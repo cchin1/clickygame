c4cfad5991ff53b775a1e7ddd30c2998908686f8
Candice Chin <chincandice2007@gmail.com>
2019-12-12 20:11:37 -0800

    completed first draft

---
+++ clickygame/src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Card.css";
+
+const Card = props => (
+  <div
+    className="card"
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container">
+      <img className={(props.score === 0 && props.message !== "Click an image to begin!") ? "shake" : ""} alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
+
+export default Card;
