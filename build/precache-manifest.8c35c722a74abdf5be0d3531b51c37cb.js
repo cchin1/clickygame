3ef987894328b16608d15e025f338ea82d85e2ed
Candice Chin <chincandice2007@gmail.com>
2019-12-14 11:24:31 -0800

    build dir

---
+++ build/precache-manifest.8c35c722a74abdf5be0d3531b51c37cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf1c18eb651aa7133d0cc4af3f0910c",
+    "url": "/Users/candice/repos/clickygame/build/index.html"
+  },
+  {
+    "revision": "9d0a46bd8eee9e871bcb",
+    "url": "/Users/candice/repos/clickygame/build/static/css/main.450b9f2b.chunk.css"
+  },
+  {
+    "revision": "12b17376e09af530068c",
+    "url": "/Users/candice/repos/clickygame/build/static/js/2.6ac3b5a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Users/candice/repos/clickygame/build/static/js/2.6ac3b5a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d0a46bd8eee9e871bcb",
+    "url": "/Users/candice/repos/clickygame/build/static/js/main.a0ca5f0a.chunk.js"
+  },
+  {
+    "revision": "638358f929d004579c1b",
+    "url": "/Users/candice/repos/clickygame/build/static/js/runtime-main.f6c91ed4.js"
+  }
+]);
