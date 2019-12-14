54135c84335537bbd023a98885e9e62c9f56321a
Candice Chin <chincandice2007@gmail.com>
2019-12-14 11:40:01 -0800

    docs dir

---
+++ docs/precache-manifest.4b04de286aec9691679af9375693bc26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d3a597c26538a118b89c31b60ba16ea",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "11ddc1e1b2fff15add4d",
+    "url": "/clickygame/static/css/main.450b9f2b.chunk.css"
+  },
+  {
+    "revision": "12b17376e09af530068c",
+    "url": "/clickygame/static/js/2.6ac3b5a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.6ac3b5a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11ddc1e1b2fff15add4d",
+    "url": "/clickygame/static/js/main.7627bd42.chunk.js"
+  },
+  {
+    "revision": "417fd3d673572284b9ca",
+    "url": "/clickygame/static/js/runtime-main.8d5882b4.js"
+  }
+]);
