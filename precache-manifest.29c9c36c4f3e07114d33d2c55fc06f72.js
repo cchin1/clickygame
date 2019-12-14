9d15abf23ac81c656f176ae5cf9963d5322f2086
Candice Chin <chincandice2007@gmail.com>
2019-12-14 10:38:05 -0800

    Updates

---
+++ precache-manifest.29c9c36c4f3e07114d33d2c55fc06f72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac8ed039bb9096e0646b5e8a135a4ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba6bfd887ac3cc2643e8",
+    "url": "/static/css/main.450b9f2b.chunk.css"
+  },
+  {
+    "revision": "12b17376e09af530068c",
+    "url": "/static/js/2.6ac3b5a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.6ac3b5a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba6bfd887ac3cc2643e8",
+    "url": "/static/js/main.fa4cbd19.chunk.js"
+  },
+  {
+    "revision": "e5decee271608f02b35a",
+    "url": "/static/js/runtime-main.96b8e03d.js"
+  }
+]);
